f277f70944a485f56c57d9ad1e0a4d80e99d1a56
jayraj <jogjayr@gmail.com>
2016-01-23 14:01:24 -0800

    Improve layout

--- app/components/Player/Player.jsx
+++ app/components/Player/Player.jsx
@@ -1,16 +1,24 @@
 import styles from './_Player.scss';
 import React from 'react';
 
-let { PropTypes } = React;
 
 export default class Player extends React.Component {
 
  
   render() {
+    let placeholderString = this.props.playerDetails.fullName.split('').map(function(char) {
+      if(char === ' ' || char === '\'') {
+        return char;
+      } else {
+        return '*';
+      }
+    }).join('');
+
     return (
-      <span>
-        {this.props.playerDetails.fullName}
-      </span>
+      <div className={styles.player}>
+        <div>{placeholderString}</div>
+        <div><input type='text' placeholder={placeholderString} /></div>
+      </div>
     );
   }
 }
--- app/components/PositionGroup/PositionGroup.jsx
+++ app/components/PositionGroup/PositionGroup.jsx
@@ -7,7 +7,7 @@ let { PropTypes } = React;
 export default class PositionGroup extends React.Component {
   render() {
     return (
-      <div>
+      <div className={styles.pos_group_layout}>
       {this.props.players.map((player) => {
         return (<Player playerDetails={player} />);
       }, this)}
