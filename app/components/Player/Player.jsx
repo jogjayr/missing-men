86dd97c2dcffdaec99d661df2860ad7a9691325e
jayraj <jogjayr@gmail.com>
2016-01-23 14:12:57 -0800

    Event handling, not going well

--- app/components/Player/Player.jsx
+++ app/components/Player/Player.jsx
@@ -4,9 +4,21 @@ import React from 'react';
 
 export default class Player extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSolved: false
+    };
+  }
+
+  handleChange(evt) {
+    if(evt.target.value === this.props.playerDetails.fullName) {
+      this.state.isSolved = true;
+    }
+  }
  
   render() {
-    let placeholderString = this.props.playerDetails.fullName.split('').map(function(char) {
+    this.state.placeholderString = this.props.playerDetails.fullName.split('').map(function(char) {
       if(char === ' ' || char === '\'') {
         return char;
       } else {
@@ -16,8 +28,8 @@ export default class Player extends React.Component {
 
     return (
       <div className={styles.player}>
-        <div>{placeholderString}</div>
-        <div><input type='text' placeholder={placeholderString} /></div>
+        <div>{this.state.placeholderString} {this.state.isSolved.toString()}</div>
+        <div><input type='text' placeholder={this.state.placeholderString} onChange={this.handleChange} /></div>
       </div>
     );
   }
